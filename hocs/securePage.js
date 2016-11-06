d2b355fa64c3ded95492b36041627d4eb5bedb7d
Luis Rudge <luis@luisrudge.net>
2016-11-06 21:33:40 -0200

    relaying getInitialProps in secure pages

--- hocs/securePage.js
+++ hocs/securePage.js
@@ -1,9 +1,21 @@
-/* eslint react/prop-types: 0 */
-import React from 'react'
+import React, { PropTypes } from 'react'
 
 import NotAuthorized from '../components/NotAuthorized'
 import defaultPage from './defaultPage'
 
-const securePagehoc = Page => props => (!props.isAuthenticated ? <NotAuthorized /> : <Page {...props} />)
+const securePageHoc = Page => class SecurePage extends React.Component {
+  static getInitialProps (ctx) {
+    return Page.getInitialProps && Page.getInitialProps(ctx)
+  }
+  static propTypes = {
+    isAuthenticated: PropTypes.bool.isRequired
+  }
+  render () {
+    if (!this.props.isAuthenticated) {
+      return <NotAuthorized />
+    }
+    return <Page {...this.props} />
+  }
+}
 
-export default Page => defaultPage(securePagehoc(Page))
+export default Page => defaultPage(securePageHoc(Page))
