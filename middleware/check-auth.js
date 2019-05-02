aab65ff57b4aa48ad5d45c6022545493203cbde5
J. Paul Bamberg <jpbamberg1993@gmail.com>
2019-05-02 15:55:49 -0400

    Replace isServer(deprecated) in favor of process.server

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -1,8 +1,8 @@
 import { getUserFromCookie, getUserFromLocalStorage } from '~/utils/auth'
 
-export default function ({ isServer, store, req }) {
-   // If nuxt generate, pass this middleware
-  if (isServer && !req) return
-  const loggedUser = isServer ? getUserFromCookie(req) : getUserFromLocalStorage()
+export default function ({ store, req }) {
+  // If nuxt generate, pass this middleware
+  if (process.server && !req) return
+  const loggedUser = process.server ? getUserFromCookie(req) : getUserFromLocalStorage()
   store.commit('SET_USER', loggedUser)
 }
