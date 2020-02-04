23913147a60437d51d36b5892ec9b4aae498abde
Sébastien Chopin <seb@nuxtjs.com>
2020-02-04 14:58:16 +0100

    fix: disable local strategy

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,6 +35,7 @@ export default {
       callback: '/auth/signed-in'
     },
     strategies: {
+      local: false,
       auth0: {
         domain: process.env.AUTH0_DOMAIN,
         client_id: process.env.AUTH0_CLIENT_ID
