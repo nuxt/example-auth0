ac853be6292cddde1eb15d509c1150ef37df6e85
Hannes Maack <hannes@maack.io>
2021-06-09 10:56:05 +0200

    upgrade to @nuxtjs/auth-next v5 + dependency updates
    Provide a working example for the recommended version v5 🔥
    Check https://auth.nuxtjs.org/providers/auth0

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,51 +1,52 @@
-import 'dotenv/config'
+import "dotenv/config";
 
 export default {
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: 'Nuxt.js + Auth0',
+    title: "Nuxt.js + Auth0",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "Nuxt.js project" }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: 'https://nuxtjs.org/favicon.ico' }
+      {
+        rel: "icon",
+        type: "image/x-icon",
+        href: "https://nuxtjs.org/favicon.ico"
+      }
     ]
   },
   /*
-  ** Global CSS
-  */
-  css: [
-    'normalize.css',
-    '@/assets/main.css'
-  ],
+   ** Global CSS
+   */
+  css: ["normalize.css", "@/assets/main.css"],
   /*
-  ** Modules
-  */
+   ** Modules
+   */
   modules: [
     // axios is required by @nuxtjs/auth
-    '@nuxtjs/axios',
+    "@nuxtjs/axios",
     // https://auth.nuxtjs.org
-    '@nuxtjs/auth'
+    "@nuxtjs/auth-next"
   ],
   auth: {
     redirect: {
-      login: '/', // redirect user when not connected
-      callback: '/auth/signed-in'
+      login: "/", // redirect user when not connected
+      callback: "/auth/signed-in"
     },
     strategies: {
-      local: false,
       auth0: {
         domain: process.env.AUTH0_DOMAIN,
-        client_id: process.env.AUTH0_CLIENT_ID
+        clientId: process.env.AUTH0_CLIENT_ID,
+        audience: process.env.AUTH0_AUDIENCE
       }
     }
   },
   build: {
     // For stormkit.io
-    publicPath: process.env.PUBLIC_PATH,
+    publicPath: process.env.PUBLIC_PATH
   }
-}
+};
